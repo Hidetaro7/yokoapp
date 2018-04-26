60031695b6139296dbd89b00e3ee2293813cc84b
Hideki Akiba <hidetaro7@gmail.com>
2018-04-26 22:56:19 +0900

    1stcmt

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/", function (req, res) {
+	fs.readFile("./data.txt", "utf-8", function (err, txt) {
+		res.send(txt);
+		console.log(txt)
+	});
+	
+})
+
+app.listen(1234);
